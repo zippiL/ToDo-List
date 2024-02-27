4479e54f4824e8345db9fe2b8288d8984e0d92b6
zippiL <150724908+zippiL@users.noreply.github.com>
2024-02-27 16:15:44 +0200

    second commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,75 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using TodoApi;
+
+///
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OpenPolicy", policy => { policy.WithOrigins("*").AllowAnyHeader()
+    .AllowAnyMethod(); });
+});
+
+builder.Services.AddDbContext<ToDoDbContext>(option=>option.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"),
+new MySqlServerVersion(new Version(8,0,36))));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+});
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
+app.UseCors("OpenPolicy");
+
+app.MapGet("/items", async (ToDoDbContext dbContext) =>
+{
+    var items = await dbContext.Items.ToListAsync();
+    return Results.Ok(items);
+});
+// הוספת משימה חדשה
+app.MapPost("/items", async (ToDoDbContext dbContext, Item item) =>
+{
+    dbContext.Items.Add(item);
+    await dbContext.SaveChangesAsync();
+    return Results.Created($"/items/{item.Id}", item);
+});
+
+// עדכון משימה
+app.MapPut("/items/{id}", async (ToDoDbContext dbContext, int id, Item item) =>
+{
+  
+
+    var existingItem = await dbContext.Items.FindAsync(id);
+    if (existingItem == null)
+    {
+        return Results.NotFound();
+    }
+
+    existingItem.Name = item.Name;
+    existingItem.IsComplete = item.IsComplete;
+    await dbContext.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+// מחיקת משימה
+app.MapDelete("/items/{id}", async (ToDoDbContext dbContext, int id) =>
+{
+    var item = await dbContext.Items.FindAsync(id);
+    if (item is null)
+    {
+        return Results.NotFound();
+    }
+
+    dbContext.Items.Remove(item);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
 
 app.Run();
